6f4590ea4195efc9748a11fe7f2674be06819ce0
alastergrume <kalinin.i.n@gmail.com>
2024-11-10 17:58:57 +0500

    change .sh

--- load_plauground.sh
+++ load_plauground.sh
@@ -1,10 +1,5 @@
 #!/bin/bush
 
-wget https://github.com/alastergrume/OllamaLocalFastApi/archive/master.zip
-sudo apt install unzip
-unzip master.zip
-rm master.zip
-cd OllamaLocalFastApi-main/
 docker build -t fastapiapp .
 docker run -d --name fastapiapp -p 8000:80 fastapiapp
 docker run -d -v ollama:/root/.ollama -p 11434:11434  ollama/ollama
