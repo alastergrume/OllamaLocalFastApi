3fc36795c1ba6669c8ac2361d32f81b358e3c4dd
alastergrume <kalinin.i.n@gmail.com>
2024-11-10 19:10:57 +0500

    change ip

--- load.sh
+++ load.sh
@@ -1,5 +1,5 @@
 #!/bin/bush
 
+docker run -d -v ollama:/root/.ollama -p 11434:11434 --name ollama ollama/ollama
 docker build -t fastapiapp .
 docker run -d --name fastapiapp -p 8000:80 fastapiapp
-docker run -d -v ollama:/root/.ollama -p 11434:11434  ollama/ollama
